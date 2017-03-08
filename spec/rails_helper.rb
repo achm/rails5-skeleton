9f8323541d6ff320cfc42ee7a9355e582244d89e
achm <achm.pppp@gmail.com>
2017-03-08 13:36:44 +0900

    configure rspec for DatabaseRewinder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -54,4 +54,19 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseRewinder['test_master']
+
+    DatabaseRewinder.clean_all
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
+end
+
+FactoryGirl.define do
+  before(:create) { SwitchPoint.writable_all! }
+  after(:create) { SwitchPoint.readonly_all! }
 end

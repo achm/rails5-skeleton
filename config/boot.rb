a0de462d536fb35f5e840dc77b5c36f368465c98
hanahiroAze <hiroyuki.hanai@gmail.com>
2017-03-10 18:55:25 +0900

    add user show

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,5 @@
+# dockerが落ちたりした時にserverが立ち上がらなくなるのを防ぐ
+File.delete(File.expand_path('../../tmp/pids/server.pid',  __FILE__)) rescue
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.

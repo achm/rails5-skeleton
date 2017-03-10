2d33fa54fa6483e5d9387007ad91d9fed826b7cb
hanahiroAze <hiroyuki.hanai@gmail.com>
2017-03-10 18:03:11 +0900

    show user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   get '/help', to:'home#help'
   get '/about', to:'home#about'
-  get  'home/about'
-  root to: 'home#index'
+  get '/contact', to: 'home#contact'
+  get '/signup',  to: 'users#new'
+  resources :users
 end
--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -16,5 +16,11 @@ RSpec.describe "Homes", type: :request do
       get "/help"
       expect(response).to have_http_status(200)
     end
+
+    it "show user" do
+      User.with_writable {User.create(name: 'before', email: 'before@example.com', password: "foobar", password_confirmation: "foobar")}
+      get "/users/1"
+      expect(response).to have_http_status(200)
+    end
   end
 end
